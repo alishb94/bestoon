10bfc94f88fc9cc09532fae3f20534366b0edf2f
alishb <ali.shb96@gmail.com>
2017-07-04 03:10:14 +0430

    expense model adde!

--- web/admin.py
+++ web/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+
 # Register your models here.
+from models import Expense
+
+admin.site.register(Expense)
--- web/models.py
+++ web/models.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
+
+
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import  User
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=225)
+    date = models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User)
